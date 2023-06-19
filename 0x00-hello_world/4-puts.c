8b8fc0b15146fee66a701cd266644f9c566f3c9f
lmtinga <lewiswambua6@gmail.com>
2023-06-19 03:01:01 -0700

    changes 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that prints a line with puts function
  * return: 0 (Success) 
  */
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
